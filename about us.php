e083b80486c24dbc721287680906961b5351524a
Dinesh Bharti <87361544+dinubharti2000@users.noreply.github.com>
2022-08-18 23:47:02 -0700

    Add files via upload

---
+++ about us.php
@@ -0,0 +1,130 @@
+<?php include "header.php" ?>
+<html lang="en">
+
+<head>
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Martel:wght@200;300;400;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- animation effect -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- csss -->
+    <link rel="stylesheet" href="./assets/css/about.css">
+
+<body>
+    <section>
+        <div class="about_main">
+            <div class="about_image">
+                <div class="container">
+                    <div class="about_heading">
+                        <h3 class="animate__animated animate__zoomIn">About Us</h3>
+                        <h5>About My Company</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="about_section">
+                <div class="container">
+                    <div class="section_about">
+                        <div class="row">
+                            <div class="col-md-7 col-sm-12">
+                                <div class="about-bg-image">
+                                    <div class="about_bg_image">
+                                        <img src="./assets/images/banner8.jpg" alt="" srcset="">
+                                        <div class="about_bg_image_2">
+                                            <img src="./assets/images/banner-1.jpg" alt="" srcset="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-5 col-sm-12">
+                                <div class="about_info" style="padding-top:75px;">
+                                    <div class="aboutus-info_heading">
+                                        <h3 class="heading-about-more">Welcome to CGC</h3>
+                                    </div>
+                                    <div class="about_info_heading2">
+                                        <h2>Since We
+                                            <br>
+                                            Started work in <br>
+                                            2022
+                                        </h2>
+                                    </div>
+                                    <div class="about_info_1">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit fugit aperiam alias id commodi harum dolores architecto illo, aspernatur optio fuga quia corporis cupiditate, animi ratione.</p>
+                                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae dolor incidunt adipisci! Facilis quidem dolores laboriosam aspernatur reprehenderit.</p>
+                                    </div>
+                                    <div class="box-1">
+                                        <a href="./about us.php">
+                                        <div class="btn btn-one">
+                                            <span>READ MORE</span>
+                                        </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- our team  -->
+            <div class="team-section p-tb80 p-b50" style="margin-top:-200px; padding-bottom:30px;">
+                <div class="team-container">
+                    <div class="team-text">
+                        <span class="text-primary text-uppercase team-our fs-5 fw-bold ">OUR BEST TEAM</span>
+                        <h3>Our TEAM</h3>
+                    </div>
+                    <div class="container team-card">
+                        <div class="card" style="width: 20rem;">
+                            <img src="./assets/images/team2.jpg" class="card-img-top" alt="...">
+                            <div class="team-card-body">
+                                <div class="teamcard-text">
+                                    <h3>nilesh</h3>
+                                    <h5>designer</h5>
+                                    <div class="team-icon">
+                                        <i class="fa-brands fa-instagram"></i>
+                                        <i class="fa-brands fa-facebook"></i>
+                                        <i class="fa-brands fa-github"></i>
+                                        <i class="fa-brands fa-twitter"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card" style="width: 20rem;">
+                            <img src="./assets/images/team2.jpg" class="card-img-top" alt="...">
+                            <div class="team-card-body">
+                                <div class="teamcard-text">
+                                    <h3>nilesh</h3>
+                                    <h5>designer</h5>
+                                    <div class="team-icon">
+                                        <i class="fa-brands fa-instagram"></i>
+                                        <i class="fa-brands fa-facebook"></i>
+                                        <i class="fa-brands fa-github"></i>
+                                        <i class="fa-brands fa-twitter"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card" style="width: 20rem;">
+                            <img src="./assets/images/team2.jpg" class="card-img-top" alt="...">
+                            <div class="team-card-body">
+                                <div class="teamcard-text">
+                                    <h3>nilesh</h3>
+                                    <h5>designer</h5>
+                                </div>
+                                <div class="team-icon">
+                                    <i class="fa-brands fa-instagram"></i>
+                                    <i class="fa-brands fa-facebook"></i>
+                                    <i class="fa-brands fa-github"></i>
+                                    <i class="fa-brands fa-twitter"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+    <!-- lottifiles -->
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</body>
+
+</html>
+<?php include "footer.php" ?>
---
+++ header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- fontawosome-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Martel:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!--animation link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <!--bootstrap css link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/product.css">
+    <link rel="stylesheet" href="./assets/css/project.css">
+    <link rel="stylesheet" href="./assets/css/header.css">
+    <link rel="stylesheet" href="./assets/css/contact.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light navbar-colour navbar-main">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a href="./index.php">
+                <div class="logo">
+                    <h4>CGC</h4>
+                    <p>Creative Glass Creation</p>
+                </div>
+                </a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto p-10px nav-bar">
+                        <li class="nav-item">
+                            <a class="nav-link " aria-current="page" href="./index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about us.php">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./product.php">Product</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="hardware.php">Hardware</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./project.php">Project</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
